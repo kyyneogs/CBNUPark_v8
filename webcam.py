99cb7802aeee2a64e1f208447bfbfd5fca599a33
kyyneogs <oks990304@gmail.com>
2023-11-06 15:59:59 +0900

    feature changed: inferences from class:ObjectDetector

--- webcam.py
+++ webcam.py
@@ -2,25 +2,34 @@ import torch
 import numpy as np
 import cv2
 from ultralytics import YOLO
+from detector import ObjectDetector
 
-
-WEIGHTS = 'yolov8m'
+# coco datasets classes: [ 3:car, 4:motorcycle, 6:bus, 8:truck ]
+CLASSES = [2, 3, 5, 7]
+MODEL = 'yolov8m'
 
 # load yolov8 model
-model = YOLO(f'weights/{WEIGHTS}.pt')
+# model = YOLO(f'weights/{MODEL}.pt')
 
-ret, cap = True, cv2.VideoCapture(0)
+# load detector
+detector = ObjectDetector(MODEL)
+
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-while ret:
+while True:
     ret, frame = cap.read()
+    assert ret
 
+    # predict object
+    results = detector.predict(frame)
+    detector.plot_bboxes(results=results, frame=frame, classes=CLASSES)
     # track object
-    results = model.track(frame, persist=True)
-    post_frame = results[0].plot()
+    # results = model.predict(frame, imgsz=640, conf=0.25, classes=CLASSES)
+    # post_frame = results[0].plot()
 
-    cv2.imshow('frame', post_frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(30)==27:
         break
 
