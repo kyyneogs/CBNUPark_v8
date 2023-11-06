8aab428e873ee55c8f995ca33c62a947affd20f1
kyyneogs <oks990304@gmail.com>
2023-11-06 14:47:02 +0900

    feature: load model, predict, plot boxes added

--- detector.py
+++ detector.py
@@ -8,8 +8,42 @@ from ultralytics import YOLO
 class ObjectDetection:
 
     def __init__(self, capture_index):
+        # object detector init
+
         self.capture_index = capture_index
+
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.model = self.load_model()
         print('Selected Device: ', self.device)
+
+        self.model = self.load_model()
+
+
+    def load_model(self):
+
+        model = YOLO('weights/yolov8n.pt')
+        model.fuse()
+
+        return model
     
+    def predict(self, frame):
+
+        results = self.model(frame)
+
+        return results
+    
+    def plot_bboxes(self, results, frame):
+
+        xyxys = []
+        confidences = []
+        class_ids = []
+
+        # Extract detections
+        for result in results:
+            boxes = result.boxes.cpu().numpy()
+
+            xyxy = boxes.xyxy
+            xywh = boxes.xywh
+
+            print(boxes)
+        
+        return frame
