ddd8f1d1d10001c650f2b8f339a450373ed11b60
kyyneogs <oks990304@gmail.com>
2023-11-08 19:23:20 +0900

    style: feat with new feature in detector

--- webcam.py
+++ webcam.py
@@ -6,12 +6,10 @@ from detector import ObjectDetector
 
 # coco datasets classes: [ 3:car, 4:motorcycle, 6:bus, 8:truck ]
 CLASSES = [2, 3, 5, 7]
-MODEL = 'yolov8m'
+MODEL = 'yolov8m.pt'
+COLOR = (255, 0, 0)
 
-# load yolov8 model
-# model = YOLO(f'weights/{MODEL}.pt')
-
-# load detector
+# load detector with yolov8 model
 detector = ObjectDetector(MODEL)
 
 cap = cv2.VideoCapture(0)
@@ -19,12 +17,13 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while True:
+    
     ret, frame = cap.read()
     assert ret
 
     # predict object
-    results = detector.predict(frame)
-    detector.plot_bboxes(results=results, frame=frame, classes=CLASSES)
+    results = detector.predict(frame, classes=CLASSES)
+    detector.plot_bboxes(results=results, frame=frame, color=COLOR)
     # track object
     # results = model.predict(frame, imgsz=640, conf=0.25, classes=CLASSES)
     # post_frame = results[0].plot()
